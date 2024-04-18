eee8912374c071980e7c1ae548812d4893bb4d0b
arif <arif@themascoteers.com>
2024-04-18 15:56:00 +1000

    done

--- fitter.py
+++ fitter.py
@@ -11,16 +11,30 @@ from utility import eval_band_rf
 class Fitter:
     def __init__(self, X, n_selected_band, img=None, gt=None):
         self.n_selected_band = n_selected_band
+        self.img = img
+        self.gt = gt
         self.model = BSNetFC()
         self.batch_size = 64
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.00002)
         self.dset = DSet(X, X)
         self.dataloader = DataLoader(self.dset, batch_size=self.batch_size, shuffle=True)
-        self.criterion = torch.nn.MSELoss(reduction='mean')
+        self.criterion = torch.nn.MSELoss(reduction='sum')
 
     def fit(self):
+        channel_weights = torch.ones((1000,200), dtype=torch.float32)
         for epoch in range(100):
             for batch, (x,y) in enumerate(self.dataloader):
+                if self.img is not None:
+                    mean_weight = torch.mean(channel_weights, dim=0)
+                    band_indx = torch.argsort(mean_weight, descending=True)[:self.n_selected_band]
+                    print(f"Selected bands: {band_indx}")
+                    x_new = self.img[:, :, band_indx]
+                    n_row, n_clm, n_band = x_new.shape
+                    img_ = minmax_scale(x_new.reshape((n_row * n_clm, n_band))).reshape((n_row, n_clm, n_band))
+                    p = Processor()
+                    img_correct, gt_correct = p.get_correct(img_, gt)
+                    score = eval_band_rf(img_correct, gt_correct)
+                    print('acc=', score)
                 channel_weights, y_hat = self.model(x)
                 mse_loss = self.criterion(y_hat, y)
                 l1_loss = self.model.get_l1_loss()
@@ -29,17 +43,6 @@ class Fitter:
                 loss.backward()
                 self.optimizer.step()
                 self.optimizer.zero_grad()
-                if img is not None:
-                    mean_weight = torch.mean(channel_weights, dim=0)
-                    band_indx = torch.argsort(mean_weight, descending=True)[:self.n_selected_band]
-                    print(f"Selected bands: {band_indx}")
-                    x_new = img[:, :, band_indx]
-                    n_row, n_clm, n_band = x_new.shape
-                    img_ = minmax_scale(x_new.reshape((n_row * n_clm, n_band))).reshape((n_row, n_clm, n_band))
-                    p = Processor()
-                    img_correct, gt_correct = p.get_correct(img_, gt)
-                    score = eval_band_rf(img_correct, gt_correct)
-                    print('acc=', score)
 
 
 if __name__ == '__main__':
